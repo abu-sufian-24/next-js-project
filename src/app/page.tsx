68df68f0a690825bfe50fd1bf7eaa673ea3e8964
abu-sufian-24 <abusufian300400@gmail.com>
2025-01-01 10:43:02 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import HeroSection from '@/components/HeroSection';
+import FeaturedSection from '@/components/FeaturedSection';
+import WhyChooseUs from '@/components/WhyChooseUs';
+import AnimationCard from '@/components/AnimationCard';
+import UpcomingWeviners from '@/components/UpcomingWeviners';
+import Inistructor from '@/components/Inistructor';
+import Footer from '@/components/Footer';
+
 export default function Home() {
-  return <h1 className="text-center text-2xl">Chai Our Code</h1>;
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-white">
+      <HeroSection />
+      <FeaturedSection />
+      <WhyChooseUs />
+      <AnimationCard />
+      <UpcomingWeviners />
+      <Inistructor />
+      <Footer />
+    </main>
+  );
 }
